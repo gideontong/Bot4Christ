3fc5fbb13694f4175d81d8bf720800823666c8aa
Gideon Tong <gideon@splimtech.com>
2019-05-10 17:55:08 -0700

    Bug fixes and another easter egg 😉😏

--- bot.js
+++ bot.js
@@ -10,6 +10,9 @@ require("./scripts/wheat.js")(bot);
 bot.on("ready", function () {
     console.log('[Info] The Discord bot has begun startup...')
     console.log(`[Info] Connected to Discord as: ${bot.user.tag} with the id: ${bot.user.id}! Prefix: ${config.prefix}, branch: ${config.branch}, version: ${config.version}`)
+    bot.user.setActivity('Puma Pants', { type: 'WATCHING' })
+        .then(presence => console.log(`[Info] Activity set to ${presence.game ? presence.game.name : 'none'}`))
+        .catch(console.error);
     require('child_process').exec('cd dashboard && node WebServer.js', () => {
     })
 });
--- scripts/wheat.js
+++ scripts/wheat.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 const config = require("../config.json");
 
 module.exports = bot => {

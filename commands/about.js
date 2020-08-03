17fa7e33d0ddc3aa9155906e71ce763a67559569
Gideon Tong <gideon@gideontong.com>
2020-08-02 20:42:55 -0700

    Scaffold permissions system

--- commands/about.js
+++ commands/about.js
@@ -1,11 +1,11 @@
-const { prefix, link } = require('../config/config.json');
+const { prefix, links } = require('../config/config.json');
 
 const { MessageEmbed } = require('discord.js');
 
 module.exports = async(bot, msg, args) => {
     const embed = new MessageEmbed()
         .setAuthor(bot.user.username, bot.user.avatarURL())
-        .setDescription(`**${bot.user.username}** by [Gideon Tong](${link}), serving ${bot.users.cache.size} Christians worldwide!
+        .setDescription(`**${bot.user.username}** by [Gideon Tong](${links.contact}), serving ${bot.users.cache.size} Christians worldwide!
             Need help? :confused: Try **${prefix}help** to get started.`)
         .setFooter(`${bot.user.username} v${process.env.npm_package_version}`)
         .setColor(0xf1d302);
--- events/message.js
+++ events/message.js
@@ -9,17 +9,35 @@ const { MessageEmbed } = require('discord.js');
 module.exports = async msg => {
     if (msg.author.bot || !msg.content.startsWith(prefix)) return;
     const bot = msg.client;
+    const args = msg.content.split(` `);
+    const command = args.shift().slice(prefix.length);
     if (msg.channel.type == 'dm') {
         const disallow = new MessageEmbed()
             .setTitle("I'm not listening to DMs yet!")
             .setDescription(`I appreciate you DMing me, :smiling_face_with_3_hearts: but I have notifications turned off. Want this feature ASAP? Contact [Gideon Tong](${links.contact}) to let him know that you want this feature.`)
             .setFooter(`${bot.user.username}`)
             .setColor(0xc0392b);
+        log.info(`${msg.author.tag} tried to send me a DM`);
         msg.channel.send(disallow);
         return;
+    } else if (permissions.commands.admin.includes(command) && (!permissions.admins.includes(msg.author) || msg.member.hasPermission('ADMINISTRATOR'))) {
+        const disallow = new MessageEmbed()
+            .setTitle("You're not allowed to do that!")
+            .setDescription(`Nuh uh uh! You have to be an admin to do that. Think this command shouldn't require admin permission? Maybe you thought you had admin powers? Contact [Gideon Tong](${links.contact}) to get this resolved.`)
+            .setFooter(`${bot.user.username} stopped you from being naughty...`)
+            .setColor(0xc0392b);
+        log.info(`${msg.author.tag} tried to run ${command} without admin permissions`);
+        msg.channel.send(disallow);
+        return;
+    } else if (permissions.commands.upcoming.includes(command) && !permissions.admins.includes(msg.author)) {
+        const disallow = new MessageEmbed()
+            .setTitle("That's coming soon...")
+            .setDescription("Try again later, Gideon is still working on this feature...")
+            .setFooter(`${bot.user.username} is too advanced!`)
+            .setColor(0xf1c40f);
+        log.info(`${msg.author.tag} tried to run upcoming command ${command}`);
+        msg.channel.send(disallow);
     }
-    const args = msg.content.split(` `);
-    const command = args.shift().slice(prefix.length);
     try {
         cmdFile = require(`../commands/${command}.js`);
     } catch {
@@ -32,7 +50,7 @@ module.exports = async msg => {
         return;
     }
     if (cmdFile) {
-        log.info(`The command ${command} has been executed by ${msg.author.tag} in ${msg.guild.name} (${msg.channel.name})`);
+        log.info(`${msg.author.tag} executed ${command} in ${msg.guild.name} (${msg.channel.name})`);
         cmdFile(bot, msg, args).catch(err => {
             const error = new MessageEmbed()
                 .setTitle("Error running " + command)

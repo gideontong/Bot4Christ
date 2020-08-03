e10ca6073bdd4e637defbd9fc9ac2e0cd935c6cf
Gideon Tong <gideon@gideontong.com>
2020-08-02 18:31:11 -0700

    Rescaffold message handler

--- events/message.js
+++ events/message.js
@@ -1,18 +1,20 @@
-const config = require('../config/config.json');
+// Imports from local config files
+const { prefix } = require('../config/config.json');
+
+// Dependencies
 const log = require('log4js').getLogger('church');
 const Discord = require('discord.js');
 
 module.exports = async msg => {
-    if (!msg.content.startsWith(config.prefix)) return;
-    if (msg.author.bot) return;
+    if (msg.author.bot || !msg.content.startsWith(prefix)) return;
     const bot = msg.client;
     const args = msg.content.split(` `);
-    const command = args.shift().slice(config.prefix.length);
+    const command = args.shift().slice(prefix.length);
     try {
-        cmdFile = require(`../../commands/${command}.js`);
+        cmdFile = require(`../commands/${command}.js`);
     } catch {
         const error = new Discord.RichEmbed()
-            .setTitle(`Hey, ${config.prefix}` + command + " isn't a command!")
+            .setTitle(`Hey, ${prefix}` + command + " isn't a command!")
             .setDescription(`Sorry about this! If you were told it's a command, please contact Gideon#5433 for more help! If you think it should be a command, contact him anyways!`)
             .setFooter(`${bot.user.username} might be going crazy...`)
             .setColor(0xf45c42)

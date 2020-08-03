618fa8071172420d6170089d57b9b195fed24b6f
Gideon Tong <gideon@gideontong.com>
2020-08-02 20:33:07 -0700

    Prevent DMs from running

--- events/message.js
+++ events/message.js
@@ -1,5 +1,5 @@
 // Imports from local config files
-const { prefix, link } = require('../config/config.json');
+const { prefix, links } = require('../config/config.json');
 const permissions = require('../config/permissions.json');
 
 // Dependencies
@@ -12,10 +12,11 @@ module.exports = async msg => {
     if (msg.channel.type == 'dm') {
         const disallow = new MessageEmbed()
             .setTitle("I'm not listening to DMs yet!")
-            .setDescription(`I appreciate you DMing me, :smiling_face_with_3_hearts: but I have notifications turned off. Want this feature ASAP? Contact [Gideon Tong](${link}) to let him know that you want this feature.`)
+            .setDescription(`I appreciate you DMing me, :smiling_face_with_3_hearts: but I have notifications turned off. Want this feature ASAP? Contact [Gideon Tong](${links.contact}) to let him know that you want this feature.`)
             .setFooter(`${bot.user.username}`)
             .setColor(0xc0392b);
         msg.channel.send(disallow);
+        return;
     }
     const args = msg.content.split(` `);
     const command = args.shift().slice(prefix.length);
@@ -24,7 +25,7 @@ module.exports = async msg => {
     } catch {
         const error = new MessageEmbed()
             .setTitle(`Hey, ${prefix}` + command + " isn't a command!")
-            .setDescription(`Sorry about this! If you were told it's a command, please contact [Gideon Tong](${link}) for more help! If you think it should be a command, contact him anyways!\n\nAlternatively, click [this link](https://github.com/gideontong/Bot4Christ/issues/new?assignees=gideontong&labels=enhancement%2C+help+wanted&template=feature-request.md&title=%5BNEW+COMMAND%5D+My+command+is...+) to submit a new feature request (*a GitHub account is required*)!`)
+            .setDescription(`Sorry about this! If you were told it's a command, please contact [Gideon Tong](${links.contact}) for more help! If you think it should be a command, contact him anyways!\n\nAlternatively, click [this link](${links.featureRequest}) to submit a new feature request (*a GitHub account is required*)!`)
             .setFooter(`${bot.user.username} might be going crazy...`)
             .setColor(0x2980b9);
         msg.channel.send(error);

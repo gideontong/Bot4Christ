0b8c93b4db29e6ae46b705a4f14af8afbef3b3c0
Gideon Tong <gideon@gideontong.com>
2020-08-02 20:00:09 -0700

    Remove QR code

--- commands/about.js
+++ commands/about.js
@@ -1,11 +1,11 @@
-const { prefix } = require('../config/config.json');
+const { prefix, link } = require('../config/config.json');
 
 const { MessageEmbed } = require('discord.js');
 
 module.exports = async(bot, msg, args) => {
     const embed = new MessageEmbed()
         .setAuthor(bot.user.username, bot.user.avatarURL())
-        .setDescription(`**${bot.user.username}** by [Gideon Tong](https://gideontong.com), serving ${bot.users.cache.size} Christians
+        .setDescription(`**${bot.user.username}** by [Gideon Tong](${link}), serving ${bot.users.cache.size} Christians
             Need help? :confused: Try **${prefix}help** to get started.`)
         .setFooter(`${bot.user.username} v${process.env.npm_package_version}`)
         .setColor(0xf1d302);
--- events/message.js
+++ events/message.js
@@ -1,33 +1,42 @@
 // Imports from local config files
-const { prefix } = require('../config/config.json');
+const { prefix, link } = require('../config/config.json');
+const permissions = require('../config/permissions.json');
 
 // Dependencies
 const log = require('log4js').getLogger('church');
-const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = async msg => {
     if (msg.author.bot || !msg.content.startsWith(prefix)) return;
+    if (msg.channel.type == 'dm') {
+        const disallow = new MessageEmbed()
+            .setTitle("I'm not listening to DMs yet!")
+            .setDescription(`I appreciate you DMing me, :smiling_face_with_3_hearts: but I have notifications turned off. Want this feature ASAP? Contact [Gideon Tong](${link}) to let him know that you want this feature.`)
+            .setFooter(`${bot.user.username}`)
+            .setColor(0xf45c42);
+        
+        msg.channel.send(disallow);
+    }
     const bot = msg.client;
     const args = msg.content.split(` `);
     const command = args.shift().slice(prefix.length);
     try {
         cmdFile = require(`../commands/${command}.js`);
     } catch {
-        const error = new Discord.MessageEmbed()
+        const error = new MessageEmbed()
             .setTitle(`Hey, ${prefix}` + command + " isn't a command!")
-            .setDescription(`Sorry about this! If you were told it's a command, please contact Gideon#5433 for more help! If you think it should be a command, contact him anyways!`)
+            .setDescription(`Sorry about this! If you were told it's a command, please contact [Gideon Tong](${link}) for more help! If you think it should be a command, contact him anyways!`)
             .setFooter(`${bot.user.username} might be going crazy...`)
-            .setColor(0xf45c42)
+            .setColor(0xf45c42);
 
-        msg.reply(error)
+        msg.channel.send(error);
     }
     log.info(`The command ${command} has been executed by ${msg.author.tag} in ${msg.guild.name}`)
     if (!cmdFile) {
         return;
-    }
-    if (cmdFile) {
+    } else {
         cmdFile(bot, msg, args).catch(err => {
-            const error = new Discord.MessageEmbed()
+            const error = new MessageEmbed()
                 .setTitle("Error running " + command)
                 .setDescription(`Sorry about this!`)
                 .addField(`The error code is `, "```" + 1 + "```", false)

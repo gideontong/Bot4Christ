a81e5d6adb4912c6bb821829ed8f0f46e8d44821
Gideon Tong <gideon@gideontong.com>
2020-08-03 01:56:13 -0700

    Switch an embed

--- commands/cases.js
+++ commands/cases.js
@@ -1,3 +1,5 @@
+const pandemicIcon = 'https://img.icons8.com/flat_round/128/000000/protection-mask.png';
+
 const https = require('https');
 const { MessageEmbed } = require('discord.js');
 const log = require('log4js').getLogger('church');
@@ -16,9 +18,15 @@ module.exports = async (bot, msg, args) => {
             res.on('end', () => {
                 data = JSON.parse(data);
                 const cases = new MessageEmbed()
-                    .setTitle('Act4Christ LIVE COVID-19 Tracker')
-                    .setDescription(`There are currently ${data[0].stats ? data[0].stats.confirmed : 'Unknown'} cases in Ventura County right now!`)
-                msg.reply(`${data[0].stats ? data[0].stats.confirmed : 'Unknown'} cases in Ventura County right now!`);
+                    .setAuthor('Stay home and wear masks!', pandemicIcon)
+                    .setTitle(`${bot.user.username} LIVE COVID-19 Tracker`)
+                    .setDescription(`There are currently **${data[0].stats ? data[0].stats.confirmed : 'Unknown'} cases** in Ventura County right now!\n*Additionally...*`)
+                    .addField('Deaths', `${data[0].stats ? data[0].stats.deaths : 'Unknown'} Deaths`, true)
+                    .addField('Recovered', `${data[0].stats ? data[0].stats.recovered : 'Unknown'} Recovered`, true)
+                    .addField('ICU Beds', `Coming Soon/97 __Currently__ In Use`, false)
+                    .setFooter(`${bot.user.username}'s data is sourced from Johns Hopkins University.`)
+                    .setColor(0x8bc34a);
+                msg.channel.send(cases);
             })
         }).end();
     } catch (err) {
--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = async (bot, msg, args) => {
         .setDescription(`Need a bit of help? ${bot.user.username} is here to help you!`)
         .addField(`To find out more about this bot... try`, `\`${prefix}about\``, false)
         .setFooter(`${bot.user.username} v${process.env.npm_package_version} Help Menu`)
-        .setColor(0xed1c24)
+        .setColor(0xed1c24);
 
     msg.channel.send(embed)
 }

d4cb87c0751f678df322f78b07feb490b5b3caee
Gideon Tong <gideon@gideontong.com>
2020-08-02 19:32:21 -0700

    Remove changeprefix as it is no longer a supported command

--- commands/about.js
+++ commands/about.js
@@ -5,7 +5,7 @@ const { MessageEmbed } = require('discord.js');
 module.exports = async(bot, msg, args) => {
     const embed = new MessageEmbed()
         .setAuthor(bot.user.username, bot.user.avatarURL())
-        .setDescription(`**${bot.user.username}** by Gideon#5433, serving ${bot.users.cache.size} Christians
+        .setDescription(`**${bot.user.username}** by [Gideon Tong](https://gideontong.com), serving ${bot.users.cache.size} Christians
             Need help? :confused: Try **${prefix}help** to get started.`)
         .setFooter(`${bot.user.username} v${process.env.npm_package_version}`)
         .setColor(0xf1d302);

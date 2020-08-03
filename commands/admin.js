9a839a5a40c677c08313b723e2c9a274e2c0ac9d
Gideon Tong <gideon@gideontong.com>
2020-08-02 19:34:27 -0700

    Scaffold a permissions config

--- commands/admin.js
+++
@@ -1,7 +0,0 @@
-module.exports = async(bot, msg, args) => {
-    if(!msg.member.roles.some(r=>["admin"].includes(r.name))) {
-        msg.reply("You aren't an admin!")
-    } else {
-        msg.reply("You're an admin!")
-    }
-}

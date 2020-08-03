7cec5410a40b7cc57bdfe8b499b6b9e9a5309b02
Gideon Tong <gideon@gideontong.com>
2020-08-02 18:38:40 -0700

    Use single quotations - didn't fix log

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 // Local config files
-const { prefix } = require("./config/config.json");
+const { prefix } = require('./config/config.json');
 const { token } = require('./config/secrets.json');
 
 // Dependencies
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const log4js = require('log4js');
 const bot = new Discord.Client();
 
 function createLogName() {
     let date = new Date();
-    return date.getFullYear() + "." + (date.getMonth() + 1) + "." + date.getDate() + "." +
-        date.getHours() + "." + date.getMinutes() + "." + date.getSeconds() + ".log";
+    return date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDate() + '.' +
+        date.getHours() + '.' + date.getMinutes() + '.' + date.getSeconds() + '.log';
 }
 
 log4js.configure({
@@ -32,9 +32,9 @@ log4js.configure({
 });
 const log = log4js.getLogger('church');
 
-require("./events.js")(bot);
+require('./events.js')(bot);
 
-bot.on("ready", function () {
+bot.on('ready', () => {
     log.info(`The bot has begun startup as ${bot.user.tag} on prefix ${prefix}`);
     bot.user.setActivity('the throne of God', { type: 'WATCHING' })
     .then(presence => log.info(`Activity set to ${presence.activites[0].type} ${presence.activities.toString()}`))

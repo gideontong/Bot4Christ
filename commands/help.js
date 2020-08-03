301ce48e298e0430c464c1cc5e9083f14032b622
Gideon Tong <gideon@gideontong.com>
2020-08-02 20:56:41 -0700

    Update help screen

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,12 @@
+const { prefix } = require("../config/config.json");
 const Discord = require("discord.js");
-const config = require("../config/config.json");
 
 module.exports = async(bot, msg, args) => {
     const embed = new Discord.MessageEmbed()
         .setAuthor(bot.user.username, bot.user.avatarURL)
         .setDescription(`Need a bit of help? ${bot.user.username} is here to help you!`)
-        .addField(`To find out more about this bot... try`, `\`${config.prefix}about\``, false)
-        .addField(`To create a QR code... try`, `\`${config.prefix}qr [string]\` with string being the text you want to make into a QR code.`, false)
-        .addField(`To find a Bible verse... try`, `\`${config.prefix}verse [verse]\`, typing out the full name of the Bible verse.`, false)
-        .setFooter(`${bot.user.username} v${config.version} Help Menu`)
+        .addField(`To find out more about this bot... try`, `\`${prefix}about\``, false)
+        .setFooter(`${bot.user.username} v${process.env.npm_package_version} Help Menu`)
         .setColor(0xed1c24)
 
     msg.channel.send(embed)

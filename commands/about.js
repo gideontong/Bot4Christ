e0585329607713c0d43dded75a94647f6637ad75
Gideon Tong <gideon@gideontong.com>
2020-08-03 22:40:29 -0700

    Fix about screen

--- commands/about.js
+++ commands/about.js
@@ -5,8 +5,7 @@ const { MessageEmbed } = require('discord.js');
 module.exports = async (bot, msg, args) => {
     const embed = new MessageEmbed()
         .setAuthor(bot.user.username, bot.user.avatarURL())
-        .setDescription(`**${bot.user.username}** by [Gideon Tong](${links.contact}), serving ${bot.users.cache.size} Christians worldwide!
-            Need help? :confused: Try **${prefix}help** to get started.`)
+        .setDescription(`**${bot.user.username}** by [Gideon Tong](${links.contact}), serving ${bot.users.cache.size} Christians worldwide!\nNeed help? :confused: Try **${prefix}help** to get started.`)
         .setFooter(`${bot.user.username} v${process.env.npm_package_version}`)
         .setColor(0xf1d302);
 

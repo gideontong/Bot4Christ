c77fc75a4e3433cab86b0983a0b28e3837602d60
Gideon Tong <gideon@gideontong.com>
2020-08-05 10:33:28 -0700

    Change one letter

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ module.exports = async (bot, msg, args) => {
         const { links } = require('../config/config.json');
         const end = new Date().valueOf();
         message.edit({
-            content: "🏓 Successfully pinged!",
+            content: "🏓 Successfully ponged!",
             embed: {
                 title: `📶 ${bot.user.username} Service Availability`,
                 description: `Currently running on \`us-west-01.gid.network/discord\`. Issues? Contact [Gideon Tong](${links.contact}) for help.`,

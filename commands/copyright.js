81e87bbedf29b16bcfdd223ce5c03ad6740f1f3a
Gideon Tong <gideon@gideontong.com>
2020-08-04 19:25:40 -0700

    Change copyright behavior with no arguments

--- commands/copyright.js
+++ commands/copyright.js
@@ -8,7 +8,19 @@ module.exports = async (bot, msg, args) => {
         .setFooter(`${bot.user.username}'s Bible Reader`)
         .setColor(0xe91e63);
     if (args.length < 1) {
-        msg.channel.send(error);
+        const message = {
+            "embed": {
+                "description": `• ${bot.user.username} is developed by [Gideon Tong](${links.contact}) and is licensed under MIT, copyright 2019-2020.\n• Various Bibles have different copyright, check their copyright with \`${prefix}copyright [version]\`\n• Usagyuuun stickers (visible on some error screens) are copyright Quan Inc. :flag_jp:`,
+                "author": {
+                    "name": bot.user.username,
+                    "icon_url": bot.user.avatarURL
+                },
+                "footer": {
+                    "text": "Copyright Information Screen"
+                }
+            }
+        }
+        msg.channel.send(message);
         return;
     }
     let version = args[0].toUpperCase();

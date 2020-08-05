4e6ebebaf0d7834a8f5a9ee55a73cf7447674c47
Gideon Tong <gideon@gideontong.com>
2020-08-05 10:19:17 -0700

    Create basic ping command

---
+++ commands/ping.js
@@ -0,0 +1,26 @@
+module.exports = async (bot, msg, args) => {
+    msg.channel.send('🏓 Pinging...').then((message) => {
+        const start = new Date().valueOf();
+        const { meta } = require('../config/bibles/KJV.json');
+        const end = new Date().valueOf();
+        message.edit({
+            embed: {
+                title: `📶 ${bot.user.username} Service Availability`,
+                fields: [
+                    {
+                        name: "Server Round Trip Time",
+                        value: `${message.createdAt - msg.createdAt}ms`
+                    },
+                    {
+                        name: "Server Connection Time",
+                        value: `${client.ping}ms`
+                    },
+                    {
+                        name: "Database Connection Time",
+                        value: `${end - start}ms`
+                    }
+                ]
+            }
+        });
+    })
+}

592a3cb17c64c55e087700878c21fd40af6c4f61
Gideon Tong <gideon@gideontong.com>
2020-08-04 17:09:26 -0700

    UJpdate bookmapper

--- lib/scripts/traverseBookmap.py
+++ lib/scripts/traverseBookmap.py
@@ -1,19 +1,31 @@
+"""
+Traverse the books and create a map of each version
+
+* Has special handling for HPB since it doesn't have all the books
+* Does not support the Acrypopha
+* Allows limited range of books, like the Torah/Hebrew Bible/Westminister
+"""
+
 import json
 import os
 
 bookmap = {}
 
 for book in json.loads(open('../../config/meta/counts.json').read()):
-    bookmap[book] = []
+    bookmap[book] = {}
+
+def set_hpb():
+    books = json.loads(open('pidginMap.json').read())
+    bookmap[book]['HPB'] = books[book]
 
 for bible in os.listdir('../../config/bibles'):
-    if 'HPB' in bible:
-        continue
     print(bible)
+    if 'HPB' in bible:
+        set_hpb()
+        continue
     books = list(json.loads(open('../../config/bibles/' + bible).read())['bible'].keys())
     mapkeys = list(bookmap.keys())
     for i in range(len(books)):
-        if books[i] not in bookmap[mapkeys[i]]:
-            bookmap[mapkeys[i]].append(books[i])
+        bookmap[mapkeys[i]][bible[:-5]] = books[i]
 
 json.dump(bookmap, open('bookmap.json', 'w'))

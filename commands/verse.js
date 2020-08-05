ddad258314a23ec7512dc9c4ed08f6b5b982deb7
Gideon Tong <gideon@gideontong.com>
2020-08-04 18:35:24 -0700

    Check for additional error handling

--- commands/verse.js
+++ commands/verse.js
@@ -23,12 +23,16 @@ module.exports = async (bot, msg, args) => {
     }
     const { meta, bible } = require(`../config/bibles/${versions['versionmap'][bibleData[0]]}`);
     let chapterName;
+    errorNotVerse.setDescription(`I couldn't find the chapter you were looking for, so if you requested a meme Bible version or a book from the apocrypha the version might not have the book. Contact [Gideon Tong](${links.contact}) if you think there's an issue.`)
     try {
         chapterName = versions['bookmap'][bibleData[1][0]][bibleData[0]];
     } catch (err) {
-        errorNotVerse.setDescription(`I couldn't find the chapter you were looking for, so if you requested a meme Bible version or a book from the apocrypha the version might not have the book. Contact [Gideon Tong](${links.contact}) if you think there's an issue.`)
         msg.channel.send(errorNotVerse);
         log.error(err);
+        return;
+    }
+    if (!chapterName) {
+        msg.channel.send(errorNotVerse);
         return;
     }
     // TODO: add verse range support

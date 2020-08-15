295d57282f4467010d5559942a970a2160ebb677
Gideon Tong <gideon@gideontong.com>
2020-08-15 00:18:57 -0700

    Add handler for automatic views

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -4,7 +4,7 @@ const { MessageEmbed } = require('discord.js');
 const log = require('log4js').getLogger('church');
 
 module.exports = async (oldMsg, newMsg) => {
-    if (oldMsg.author.bot || newMsg.author.bot) return;
+    if (oldMsg.author.bot || newMsg.author.bot || !newMsg.editedAt) return;
     log.info(`${oldMsg.author.tag} edited ${oldMsg.cleanContent} to ${newMsg.cleanContent} in ${oldMsg.guild.name} (${oldMsg.channel.name})`);
     let guildLog = oldMsg.client.guilds.resolve(logger.guild);
     try {

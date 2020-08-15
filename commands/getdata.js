a5c168724d34b585b29e0b3f2922e32cc42bf788
Gideon Tong <gideon@gideontong.com>
2020-08-15 10:18:34 -0700

    Complete Bible task handler functions

--- commands/getdata.js
+++ commands/getdata.js
@@ -1,5 +1,5 @@
-const { verseToday } = require('../lib/Bible');
+const { query } = require('../lib/Bible');
 
 module.exports = async (bot, msg, args) => {
-    msg.channel.send(verseToday());
+    msg.channel.send(query(msg.cleanContent.substring(args[0].length + 1)));
 }

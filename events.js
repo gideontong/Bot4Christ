09f170f4bb37b38f5f3a382f9403834d07f9133a
Gideon Tong <gideon@gideontong.com>
2020-08-14 21:28:15 -0700

    Add messageUpdate event emit

--- events.js
+++ events.js
@@ -2,4 +2,5 @@ const reqEvent = (event) => require(`./events/${event}`);
 module.exports = bot => {
     bot.on('message', reqEvent('message'));
     bot.on('messageDelete', reqEvent('messageDelete'));
+    bot.on('messageUpdate', reqEvent('messageUpdate'));
 }
---
+++ events/messageUpdate.js
@@ -0,0 +1,27 @@
+const { logger } = require('../config/config.json');
+
+const { MessageEmbed } = require('discord.js');
+const log = require('log4js').getLogger('church');
+
+module.exports = async (oldMsg, newMsg) => {
+    if (oldMsg.author.bot || newMsg.author.bot) return;
+    log.info(`${oldMsg.author.tag} edited ${oldMsg.cleanContent} to ${newMsg.cleanContent} in ${oldMsg.guild.name} (${oldMsg.channel.name})`);
+    let guildLog = msg.client.guilds.resolve(logger.guild);
+    try {
+        if (guildLog.available) {
+            let channelLog = guildLog.channels.resolve(logger.channel);
+            if (channelLog && channelLog.type == 'text') {
+                const editedComment = new MessageEmbed()
+                    .setAuthor(oldMsg.author.tag, oldMsg.author.displayAvatarURL())
+                    .setTitle(`Message edited in ${msg.guild.name}!`)
+                    .setDescription(`It had ${oldMsg.attachments.size} attachments.`)
+                    .addField('Old Message', oldMsg.cleanContent, false)
+                    .addField('New Message', newMsg.cleanContent, false)
+                    .setFooter(`Edited at ${newMsg.editedAt} in ${oldMsg.channel.name}`);
+                channelLog.send(editedComment);
+            }
+        }
+    } catch (err) {
+        log.error(`While trying to emit a messageUpdate I got ${err}`);
+    }
+}

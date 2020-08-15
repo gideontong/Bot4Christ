cbfb8f55b14dadbb3185eb0e4446fc5228dc4d32
Gideon Tong <gideon@gideontong.com>
2020-08-15 10:56:25 -0700

    Finish buildEmbed

--- commands/getdata.js
+++ commands/getdata.js
@@ -1,5 +1,3 @@
-const { query } = require('../lib/Bible');
-
 module.exports = async (bot, msg, args) => {
-    msg.channel.send(query(msg.cleanContent.substring(args[0].length + 1)));
+    msg.channel.send('Command is currently disabled!');
 }

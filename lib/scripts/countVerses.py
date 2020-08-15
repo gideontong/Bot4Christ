e679dc24c925293ee42d52582b594b3445481b56
Gideon Tong <gideon@gideontong.com>
2020-08-15 01:23:16 -0700

    Update counts

--- lib/scripts/countVerses.py
+++ lib/scripts/countVerses.py
@@ -1,13 +1,14 @@
 import json
 
 bible = json.loads(open('NIV.json').read())['bible']
+names = json.loads(open('bible.json').read())['bookmap']
 meta = {
 }
 # 66
-for book in bible:
+for book in names.keys():
     meta[book] = {}
-    for chapter in bible[book]:
-        meta[book][chapter] = len(bible[book][chapter])
+    for chapter in bible[names[book]['NIV']]:
+        meta[book][chapter] = len(bible[names[book]['NIV']][chapter])
 
-with open('counts.json', 'w') as file:
+with open('counts_updated.json', 'w') as file:
     json.dump(meta, file)

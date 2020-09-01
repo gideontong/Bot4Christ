3fa539ec387b579c329ed6e090703fd6cedd5e69
Gideon Tong <gideon@gideontong.com>
2020-08-31 17:35:08 -0700

    Enable bot events

--- events.js
+++ events.js
@@ -1,5 +1,7 @@
 const reqEvent = (event) => require(`./events/${event}`);
 module.exports = bot => {
+    bot.on('guildCreate', reqEvent('guildCreate'));
+    bot.on('guildDelete', reqEvent('guildDelete'));
     bot.on('message', reqEvent('message'));
     bot.on('messageDelete', reqEvent('messageDelete'));
     bot.on('messageUpdate', reqEvent('messageUpdate'));

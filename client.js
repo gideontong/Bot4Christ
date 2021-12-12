ae00ea91a35254a66da80ee48251212d86c23fab
Gideon Tong <gideon@gideontong.com>
2021-12-12 01:02:39 -0800

    Bring back statuses

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const log4js = require('log4js');
 
 // Configuration
+const { activity } = require('./config/config.json');
 const { token } = require('./config/secrets.json');
 const loggingConfig = require('./config/logging.json');
 
@@ -19,13 +20,18 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
+  client.commands.set(command.data.name, command);
 }
 
 require('./events.js')(client);
 
 client.once('ready', () => {
-	logger.info(`Logged in as ${client.user.tag}!`);
+  logger.info(`Logged in as ${client.user.tag}!`);
+
+  setInterval(() => {
+    const idx = Math.floor(Math.random() * activity.activities.length);
+    client.user.setActivity(activity.activities[idx].text, activity.activities[idx].options);
+  }, activity.updateInterval * 1000);
 });
 
 client.login(token);

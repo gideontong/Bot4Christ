16c116939a613585a9b76438f767490eea51a96d
Gideon Tong <gideon@gideontong.com>
2021-12-11 22:35:04 -0800

    Add new code

--- commands/versions.js
+++ commands/versions.js
@@ -1,12 +1,53 @@
+const { MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const { availableVersions } = require('../config/bible/config.json');
+const { availableVersions, files } = require('../config/bible/config.json');
+
+const colors = 0xFFFFFF;
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('versions')
-    .setDescription('See a list of available versions'),
+    .setDescription('See a list of available versions')
+    .addStringOption(option =>
+      option.setName('version')
+        .setDescription('Bible version or language')
+        .setRequired(false)
+    ),
   async execute(interaction) {
-    interaction.reply('Coming soon!');
+    let query = interaction.options.getString('version');
+    const color = Math.floor(Math.random() * colors);
+
+    if (query) {
+      query = query.toUpperCase();
+
+      if (!(query in availableVersions)) {
+        interaction.reply({
+          content: 'That isn\'t a valid Bible version!',
+          ephemeral: true
+        });
+        return;
+      }
+
+      const embed = new MessageEmbed()
+        .setTitle(`About the ${query}`)
+        .setColor(color);
+
+      interaction.reply({
+        embeds: [embed]
+      });
+      return;
+    }
+
+    const versions = Object.keys(availableVersions);
+    const embed = new MessageEmbed()
+      .setTitle('Available Versions')
+      .setColor(color)
+      .setDescription(versions.join(', '))
+      .setFooter('Use /version CODE to find out more about a specific version.');
+
+    interaction.reply({
+      embeds: [embed]
+    });
   },
 };
